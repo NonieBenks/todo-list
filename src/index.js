e3f7b8a0a8f1755568cf694357172a69048606ef
Natalia Benko <characteronme@gmail.com>
2024-03-31 12:18:14 +0300

    feat: init commit;

---
+++ src/index.js
@@ -0,0 +1,12 @@
+class SideQuest {
+  constructor(name, description, dueDate, priority, notes, checklist) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+  }
+
+  buldNewQuest(name, description, dueDate, priority, notes, checklist) {}
+}
